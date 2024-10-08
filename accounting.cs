a609448aa6ace0482543d8490cf12ca12741a819
Donovan Lundrigan <donavon892@gmail.com>
2024-10-07 22:51:48 -0300

    initial commit for projects

---
+++ accounting.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Acme Accounting Systems. \nRemember, we're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
