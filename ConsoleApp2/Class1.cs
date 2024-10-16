9361568a0296b42ed31b5ab0a858dc2092ed0a65
Donovan Lundrigan <donavon892@gmail.com>
2024-10-16 13:57:35 -0300

    main method assignment#

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    public class Class1
+    {
+        public int Operation(int num) //first method to take in an integer
+        {
+            return num / 7;
+        }
+        public int Operation(decimal num) //second method to take in a decimal
+        {
+            int newNum = Convert.ToInt32(num);
+            return newNum * 22;
+        }
+        public int Operation(string num) //third method to take in a string
+        {
+            int newNum = Convert.ToInt32(num);
+            return newNum - 33;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1(); //instantiate the class
+            Console.WriteLine(class1.Operation(9)); //calls method 1 with int param
+            Console.WriteLine(class1.Operation(14.73m)); //calls method 2 with decimal param
+            Console.WriteLine(class1.Operation("15")); //calls method 3 with string param
+            Console.ReadLine(); //keeps window open until user hits enter/exits
+        }
+    }
+}
