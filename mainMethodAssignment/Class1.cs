e9e6d4cef81a95887e74239f6a5f5d408002f47c
Donovan Lundrigan <donavon892@gmail.com>
2024-10-16 13:44:38 -0300

    Main Method Assignment

---
+++ mainMethodAssignment/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace mainMethod
+{
+    public class Class1
+    {
+        public int Operation(int num) //first method to take in an integer
+        {
+            return num / 7;
+        }
+        public int Operation(decimal num) //second method to take in a decimal
+        {
+            int newNum = Convert.ToInt32(num);
+            return newNum * 22;
+        }
+        public int Operation(string num) //third method to take in a string
+        {
+            int newNum = Convert.ToInt32(num);
+            return newNum - 33;
+        }
+    }
+}
