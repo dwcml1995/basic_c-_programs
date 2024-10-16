0c7c21a8972c77fda321fe9d6dfd4aaef7d1c8dd
Donovan Lundrigan <donavon892@gmail.com>
2024-10-16 14:22:42 -0300

    Method Assignment

---
+++ methodAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment
+{
+    public class Class1
+    {
+        public int Operation(int num, int num2 = 8) //created method with one optional parameter
+        {
+            int result = num * num2; //perform a math operation
+            return result;
+        }
+    }
+}
