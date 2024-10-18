2d1fa685fceabe58b47efcbf5c97e9917bbf8573
Donavon <donavon892@gmail.com>
2024-10-18 12:19:03 -0300

    Update employee.cs

--- employeesPeopleAssignment/employee.cs
+++ employeesPeopleAssignment/employee.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace employees_people
 {
-    public class employee : person //employee class that inherits props from person class
+    public class employee : person, IQuittable //employee class that inherits props from person class
     {
-        public int id {  get; set; } //set the property for id
+        public int id { get; set; } //set the property for id
+
+        public override void sayName() //implemented the sayname method in the class 
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+        public void Quit() //implemented the quit method 
+        {
+            Console.WriteLine($"{firstName} {lastName} has quit.");
+        }
     }
-    public virtual void SayName() //implemented the sayname method in the class 
 }
