b529267f0e6142b6cba4b777fbf7f0c7fac980d7
Donovan Lundrigan <donavon892@gmail.com>
2024-10-21 15:31:51 -0300

    struct assignment

---
+++ parseEnum/structAssignment/Number.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace structAssignment
+{
+    public struct Number //defines struct
+    {
+        public decimal Amount; //properties 
+
+        public Number(decimal amount) //construction that initializies the amount
+        {
+            Amount = amount;
+        }
+    }
+}
---
+++ parseEnum/structAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number(8.92m); //creates an object of the data type Number and assigns an amount to it in decimal 
+            Console.WriteLine($"The amount is: {num1.Amount}"); //prints the amount
+            Console.ReadLine(); //keeps console open 
+        }
+    }
+}
