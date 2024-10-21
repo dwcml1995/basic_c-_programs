3bc5267a9779d7a55949513ed3c09e7089e918ee
Donavon <donavon892@gmail.com>
2024-10-21 12:07:18 -0300

    Update employee.cs

--- employeesPeopleAssignment/employee.cs
+++ employeesPeopleAssignment/employee.cs
@@ -8,15 +8,29 @@ namespace employees_people
 {
     public class employee : person, IQuittable //employee class that inherits props from person class
     {
-        public int id { get; set; } //set the property for id
+        public int id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public override void sayName() //implemented the sayname method in the class 
         {
-            Console.WriteLine("Name: " + firstName + lastName);
+            Console.WriteLine("Name: " + FirstName + LastName);
         }
         public void Quit() //implemented the quit method 
         {
             Console.WriteLine($"{firstName} {lastName} has quit.");
+        }
+        public static bool operator ==(employee employee1, employee employee2) //overloading == operator to compare employees by id 
+        {
+            if (ReferenceEquals(employee1, employee2)) //if both are null or the same reference, it will return true 
+                return true;
+            if (employee1 is null || employee2 is null) //if one is null, returns false 
+                return false;
+            return employee1.id == employee2.id; //compares the properties of each employee 
+        }
+        public static bool operator !=(employee employee1, employee employee2) //overloading != to be the opposite of teh == operator 
+        {
+            return !(employee1 == employee2); 
         }
     }
 }
