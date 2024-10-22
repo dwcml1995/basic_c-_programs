7e7cb44b24a17166a10c3e2d384d76c87b6f6a15
Donovan Lundrigan <donavon892@gmail.com>
2024-10-22 11:01:54 -0300

    lambda expression assignment

---
+++ lambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaExpressions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<employee> employees = new List<employee>(); //instantiated employee class as a list & created 10 employees
+            new employee() { ID = 1, FirstName = "Billy", LastName = "Bob" };
+            new employee() { ID = 2, FirstName = "Joe", LastName = "Schmo" };
+            new employee() { ID = 3, FirstName = "Itachi", LastName = "Uchiha" };
+            new employee() { ID = 4, FirstName = "Joe", LastName = "Beninati" };
+            new employee() { ID = 5, FirstName = "Jarad", LastName = "Higgins" };
+            new employee() { ID = 6, FirstName = "Symere", LastName = "Woods" };
+            new employee() { ID = 7, FirstName = "Joey", LastName = "Kangaroo" };
+            new employee() { ID = 8, FirstName = "Donavon", LastName = "William" };
+            new employee() { ID = 9, FirstName = "Joe", LastName = "Throw" };
+            new employee() { ID = 10, FirstName = "Last", LastName = "One" };
+           
+            List<employee> joes = new List<employee>(); //created a list for employees who are named job
+            foreach (employee employee in employees) //using a foreach loop to achieve this
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee); //if the employee is named joe, they are added to the list
+                }
+            }
+            List<employee> joe = employees.Where(x => x.FirstName == "Joe").ToList(); //lambda to achieve the same thing 
+            List<employee> IdOver5 = employees.Where(x => x.ID >= 5).ToList(); //lambda to find employees who ID is equal to or greater than 5 
+            Console.ReadLine(); //keeps console open
+        }
+    }
+}
---
+++ lambdaExpressions/employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaExpressions
+{
+    public class employee
+    {
+        public int ID { get; set; } //propertiees for ID 
+        public string FirstName { get; set; } //properties for first name
+        public string LastName { get; set; } //properties for last name
+    }
+}
